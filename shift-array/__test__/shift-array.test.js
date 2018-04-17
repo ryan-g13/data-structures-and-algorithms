a066ef5d7f95d756ef5e867761f67db00c01ad4a
Ryan Groesch <ryan.groesch@gmail.com>
2018-04-17 16:59:06 -0700

    push for freedom from character limit

--- shift-array/__test__/shift-array.test.js
+++ shift-array/__test__/shift-array.test.js
@@ -6,7 +6,8 @@ describe('Test cases to verify shift Array method works', () => {
   test('Even number of indicies in array', () => {
     const testArr1 = [1, 3, 5, 7, 9, 11, 13, 15];
     const testValue1 = 8;
-    /* eslint-disable-line */ expect(shiftArray.insertShiftArray(testArr1, testValue1)).toEqual([1, 3, 5, 7, 8, 9, 11, 13, 15]);
+    expect(shiftArray.insertShiftArray(testArr1, testValue1)) 
+      .toEqual([1, 3, 5, 7, 8, 9, 11, 13, 15]);
   });
   test('Odd number of indicies in array', () => {
     const testArr2 = [1, 3, 5];
