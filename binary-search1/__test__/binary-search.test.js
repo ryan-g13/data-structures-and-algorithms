9d1f92eae6a5f9a1c8c91a37399b6213a86f8409
Ryan Groesch <ryan.groesch@gmail.com>
2018-04-18 15:37:45 -0700

    whiteboarding done

---
+++ binary-search1/__test__/binary-search.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const binarySearch = require('../binary-search.js');
+
+describe('Test cases to verify if binary search is correctly returning values either index of search or -1 if DNE', () => {
+  test('Test to verify if can select the correct index if included', () => {
+    const testVal = 13;
+    const testArr = [3, 5, 7, 11, 13, 17, 19];
+    expect(binarySearch.binarySearch(testArr, testVal)).toEqual(4);
+  });
+  test('Test case to verify if value is not included', () => {
+    const testVal1 = 99;
+    const testArr1 = [1, 1, 2, 3, 5, 11];
+    expect(binarySearch.binarySearch(testArr1, testVal1)).toEqual(-1);
+  });
+});
