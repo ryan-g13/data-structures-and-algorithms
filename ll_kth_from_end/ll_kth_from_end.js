79889bb4eb592934a07c7b9c5dbe796d629aa2b5
Ryan Groesch <ryan.groesch@gmail.com>
2018-04-25 08:55:22 -0700

    Whiteboard done

---
+++ ll_kth_from_end/ll_kth_from_end.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const Node = require('./node');
+
+// Assumption from whiteboarding test that this exists/ we have access to this.
+module.exports = class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  kthFromEnd(k) {
+    if (!this.head) {
+      return new Error('The Linked List DNE');
+    }
+    let currentNode = this.head;
+    let count = 0;
+    while (currentNode.next) {
+      count += 1;
+      currentNode = currentNode.next;
+    }
+    const length = count; 
+    const targetNode = length - k;
+    if (targetNode < 0) return new Error('K is outside of LL please submit new K value');
+    currentNode = this.head;
+    for (let i = 0; i < targetNode; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  // Assumption of access from whiteboarding.
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  // Assumption of access from whiteboarding question.
+  insertBefore(value, newValue) {
+    if (!this.head) {
+      throw new Error('__ERROR__ List is empty');
+    }   
+    
+    if (this.head.value === value) {
+      const temp = this.head;
+      this.head = new Node(newValue);
+      this.head.next = temp;
+      return this;
+    }
+    const currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        const temp = currentNode.next;
+        currentNode.next = new Node(newValue);
+        currentNode.next.next = temp;
+        break;
+      }
+    }
+    if (currentNode.value !== value) return new Error('Node Value Not in linked List');
+    return this;
+  }
+
+  // Assumption of access from whiteboarding question.
+  insertAfter(value, newValue) {
+    if (!this.head) {
+      throw new Error('__ERROR__ List is empty');
+    }
+
+    if (this.head.value === value) {
+      const temp = this.head.next;
+      this.head.next = new Node(newValue);
+      this.head.next.next = temp;
+      return this;
+    }
+    const currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.value === value) {
+        const temp = currentNode.next;
+        currentNode.next = new Node(newValue);
+        currentNode.next.next = temp;
+        break; 
+      }
+    }
+    if (currentNode.value !== value) return new Error('Node Value Not in linked List');
+    return this;
+  }
+};
---
+++ ll_kth_from_end/node.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Assumption given from whiteboarding text
+module.exports = class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+};
