9ec7768039a006fe11c8e4e4411386cefa24c404
Ryan Groesch <ryan.groesch@gmail.com>
2018-06-06 08:43:32 -0700

    quicksort

--- ll_merge/ll_function.js
+++ ll_merge/ll_function.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const list = module.exports = {};
-
-function mergeList(linkedList1, linkedList2) {
+module.exports = function mergeList(linkedList1, linkedList2) {
   let currentNode = linkedList1.head;
   let temp2 = linkedList2.head;
   if (!currentNode || !temp2) return new Error('One or more linked lists DNE');
@@ -18,4 +16,4 @@ function mergeList(linkedList1, linkedList2) {
     if (currentNode.next === null || temp2.next === null) return linkedList1.head;
   }
   return linkedList1.head;
-}
+};
---
+++ quick-sort/README.md
@@ -0,0 +1,7 @@
+# Quick Sort Method 
+
+## Challenge
+Write a function that accepts an array of integers, and returns an array sorted by a recursive quicksort algorithm.
+
+## Solution
+![WhiteBoardImage](./assets/401-WB-37.jpg)
